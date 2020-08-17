34684222fa2d0e9d528355ef30ab6b258568707e
WinterCricket <hodhodhod2003@gmail.com>
2020-08-17 00:09:23 -0500

    add an include func

--- includes.php
+++ includes.php
@@ -6,22 +6,7 @@
 </head>
 <body>
 	<h1>PHP includes and PHP configurations</h1>
-	<?php 
-
-		/*
-
-		PHP ini settings:
-		date.timezone = UTC
-		Error handling and logging - > display_errors = On
-			extension_dir = "C:\mamp\bin\php7.2.14\ext\"
-
-		*/
-
-		$abc = "Montreal";
-
-		echo $qwery;
-		// echo $abc;
-	 ?>
+	<?php include(); ?>
 	
 </body>
 </html>
