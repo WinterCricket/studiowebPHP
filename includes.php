59b191256bf0a6db6a180c4283d199e5a80dfd18
WinterCricket <hodhodhod2003@gmail.com>
2020-08-16 23:53:30 -0500

    use ini file to turn on error display for php

---
+++ includes.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>PHP includes and PHP configurations</title>
+</head>
+<body>
+	<h1>PHP includes and PHP configurations</h1>
+	<?php 
+
+		/*
+
+		PHP ini settings:
+		date.timezone = UTC
+		Error handling and logging - > display_errors = On
+			extension_dir = "C:\mamp\bin\php7.2.14\ext\"
+
+		*/
+
+		$abc = "Montreal";
+
+		echo $qwery;
+		// echo $abc;
+	 ?>
+	
+</body>
+</html>
