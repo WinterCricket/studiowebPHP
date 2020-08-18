d44539b92f7b67c01aaa2a5da29bb0ee9fb15226
WinterCricket <hodhodhod2003@gmail.com>
2020-08-18 03:03:18 -0500

    include some include css inphp and try images

--- includes.php
+++ includes.php
@@ -6,7 +6,16 @@
 </head>
 <body>
 	<h1>PHP includes and PHP configurations</h1>
-	<?php include(); ?>
+	<?php include_once "style.php"; ?>
+
+	
+	<?php include "images/miami-trans.png"; ?>
+
+	<?php  /*https://www.bailbondshq.com/texas/harrison-inmate-KNUTSON/10733-2020 ?>
+	https://www.bailbondshq.com/texas/harrison-inmate-CSASZAR/58796
+
+	*/
+	?>
 	
 </body>
 </html>
