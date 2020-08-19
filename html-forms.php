7c0128dd3e4cdef2b754e1fc14eb0223442cfdba
WinterCricket <hodhodhod2003@gmail.com>
2020-08-18 21:50:29 -0500

    send via post

---
+++ html-forms.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>PHP Form Processing</title>
+</head>
+<body>
+	<h1>PHP Form Processing</h1>
+	
+<?php include "style.php" ?>
+	
+	<form method="GET" action="form-response.php">
+
+		Name: <input type="text" name="name">
+		<br>
+		Password: <input type="password" name="password">
+		<br>
+		<input type="submit">
+		
+
+	</form>
+	
+
+	
+	
+</body>
+</html>
