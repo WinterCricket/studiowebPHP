6c3e1642f0f7de78466e315e777b059c27fb50e2
WinterCricket <hodhodhod2003@gmail.com>
2020-08-22 00:52:44 -0500

    create functions file and set up page

---
+++ functions.php
@@ -0,0 +1,29 @@
+<?php 
+
+$message = 5 + 6;
+
+ ?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>PHP Functions</title>
+</head>
+<body>
+	<h1>PHP Functions</h1>
+	<?php include "style.php"; ?>
+
+	<?php 
+
+	echo "<h1>$message</h1>";
+	 ?>
+
+	
+	
+
+	
+	
+</body>
+</html>
