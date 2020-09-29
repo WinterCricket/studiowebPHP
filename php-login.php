d4a3f4f10605bbdedfab201247aaf34ca37d9e97
WinterCricket <hodhodhod2003@gmail.com>
2020-09-29 00:14:09 -0500

    add login files and set login home page

--- php-login.php
+++ php-login.php
@@ -1,14 +1,55 @@
 <?php 
-	include "style.php";
- ?>
- <!DOCTYPE html>
- <html lang="en">
- <head>
- 	<meta charset="UTF-8">
- 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
- 	<title>Writer Club Login</title>
- </head>
- <body>
- 	<h1>Writer Club Login</h1>
- </body>
- </html>
+include "style.php";
+session_start();
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>form processing</title>
+</head>
+<body>
+	<h1>Writer Club Login</h1>
+
+	<nav>
+		<ul>
+			<li><a href="php-login.php">Home</a></li>
+			<li><a href="protected-page.php">Protected page</a></li>
+			<li><a href="#">Contact</a></li>
+			<li><a href="logout.php">Log Out</a></li>
+		</ul>
+	</nav>
+
+	<main>
+
+
+
+		<?php 
+		if(isset($session['isLoggedIn'])) {
+		//if the user logged in, don't show form and confuse them
+			echo "<p>You are logged in.</p>";
+		} else{
+		//using Heredoc to echo out form
+			$theForm = <<<THEFORM
+
+			<p>Welcome to Writer Club </p>
+			<h2>Please enter user name and password to log in:</h2>
+
+			<form method = "post" action="login-response.php">
+
+				<input type="text" name = "userName" id = "username">
+				<input type="password" name = "password">
+				<input type="submit">
+			</form>
+THEFORM;
+
+			echo $theForm;
+
+		}
+
+		?>
+		</main>
+
+		</body>
+		</html>
--- style.php
+++ style.php
@@ -139,13 +139,8 @@
 	}
 </style>
 
-<h1>CSS Navbars</h1>
 
 
-<ul>
-	<li><a href="#home">Home</a></li>
-	<li><a href="#news">News</a></li>
-	<li><a href="#contact">Contact</a></li>
-	<li><a href="#about">About</a></li>
-</ul>
+
+
 
